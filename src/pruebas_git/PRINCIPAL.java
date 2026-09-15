2dbc7fcf62dc462c47b6a48b113f5e3ba0f00991
Jorge Crespo <jcrespoespada@gmail.com>
2026-09-15 10:02:44 +0200

    Corregir carpeta src y actualizar proyecto

---
+++ src/pruebas_git/PRINCIPAL.java
@@ -0,0 +1,13 @@
+package pruebas_git;
+
+public class PRINCIPAL {
+
+	public static void main(String[] args) {
+		System.out.println("HOLA QUE TAL");
+		System.out.println("jjbvhgfgjkhkjgbuytg");
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//test
